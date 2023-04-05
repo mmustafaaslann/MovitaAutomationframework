2274e3f44557ee06868606a344dda202dffb4e25
Mustafa ASLAN <mmustafaslann1453@gmail.com>
2023-04-05 10:20:03 +0300

    SCENARIO yazıldı

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 @CucumberOptions(
 
         //features = {"c:/features"},                   // feature'lar genellikle test/resources altinda olur
-        features = {"src/test/java/resources/features/Automation_Anasayfa_Suleyman.feature"},         // feature yolu alinirken content rootdan baslanir
+        features = {"src/test/java/resources/features/Automation_Anasayfa_Mustafa.feature"},         // feature yolu alinirken content rootdan baslanir
         glue = {"stepdefs"},        // glue'lar, stepdefs ve hooks classlarinin yeridir, sourceroot'dan baslar
         dryRun = false,           // sadece step defsler düzgün hazirlanmismi, yapilandirma uygun mu
         tags = "@login",         // @login tagli senaryolar calistirilin
